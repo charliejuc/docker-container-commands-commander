f78be0de169803577688dd6896ddf680c5178945
charliejuc <charlieinsous@gmail.com>
2020-07-10 08:43:00 +0200

    Adds extra_args variable.

--- commander-container.sh
+++ commander-container.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-image=$1
+extra_args=$1
+image=$2
 
 if [ ! $image ]; then
-    >&2 echo "image parameter is required"
-    exit 1
+    if [ ! "$extra_args" ]; then
+        >&2 echo "'image' parameter is required"
+        exit 1
+    fi
+
+    image=$extra_args
 fi
 
 #adds your custom volumes in -v flag
-docker run -d --rm --net=host \
+docker run -d --rm --net=host $extra_args \
     -v /var/example:/var/example
     --name commander $image tail -f /dev/null
